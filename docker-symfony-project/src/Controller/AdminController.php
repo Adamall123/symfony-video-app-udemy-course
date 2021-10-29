858a92c0db542898407b6154de882e27cadf690d
Adam Wojdylo <adam.wojdylo1234@gmail.com>
2021-10-29 19:50:48 +0200

    add links to routes in admin template and create methods for them

--- docker-symfony-project/src/Controller/AdminController.php
+++ docker-symfony-project/src/Controller/AdminController.php
@@ -19,5 +19,40 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/my_profile.html.twig');
     }
-    
+
+    /**
+     * @Route("/categories", name="categories")
+     */
+    public function categories(): Response
+    {
+        return $this->render('admin/categories.html.twig');
+    }
+    /**
+     * @Route("/upload_video", name="upload_video")
+     */
+    public function upload_video(): Response
+    {
+        return $this->render('admin/upload_video.html.twig');
+    }
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users(): Response
+    {
+        return $this->render('admin/users.html.twig');
+    }
+    /**
+     * @Route("/videos", name="videos")
+     */
+    public function videos(): Response
+    {
+        return $this->render('admin/videos.html.twig');
+    }
+    /**
+     * @Route("/edit-category", name="edit_category")
+     */
+    public function editCategroy(): Response
+    {
+        return $this->render('admin/edit_category.html.twig');
+    }
 }
